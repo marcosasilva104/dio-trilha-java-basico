cc6c47fa0d3aed596673605688f8650d4ffb4e02
marcosasilva104 <marcosasilva104@gmail.com>
2024-07-10 08:07:31 -0300

    Criando variáveis de direrentes tipos

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,13 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;    
+        double salarioMinimo = 2500.33;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
