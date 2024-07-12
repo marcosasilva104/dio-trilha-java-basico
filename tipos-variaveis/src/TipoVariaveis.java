cbb356a2f942d53b1463e0793d1bb99eb6471f47
marcosasilva104 <marcosasilva104@gmail.com>
2024-07-11 21:48:12 -0300

    Projeto conceito basico P.O.O

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,13 +1,23 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
+        
+        String nome = "Marcos A. da Silva";
         byte idade = 123;
         short ano = 2021;
         int cep = 21070333;
         long cpf = 98765432109L;
-        float pi = 3.14F;    
+        final float VALOR_DE_PI = 3.14F;    
         double salarioMinimo = 2500.33;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println("O novo número é : " + numero);
+
+        System.out.println("O valor de PI é : " + VALOR_DE_PI);
     }
 }
