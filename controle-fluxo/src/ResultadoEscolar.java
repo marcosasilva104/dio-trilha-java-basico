316e6519c784c11269b9e2cedfca81bf2e86f96b
marcosasilva104 <marcosasilva104@gmail.com>
2024-07-12 20:49:36 -0300

    Inclusão de mais uma condição na condição ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -10,7 +10,7 @@ public class ResultadoEscolar {
         else
             System.out.println("Reprovado");
     
-        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >=7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
 
         System.out.println(resultado);
     }
