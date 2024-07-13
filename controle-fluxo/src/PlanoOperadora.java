db23ed16b4a79fde69c2fdeb9ed4515f4d467fdd
marcosasilva104 <marcosasilva104@gmail.com>
2024-07-12 21:05:47 -0300

    Inclusão da classe PlanoOperadora para treinar o swith case

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,15 @@
+public class PlanoOperadora {
+
+    public static void main(String[] args) {
+        String plano = "B";
+
+        switch (plano) {
+            case "T":
+                System.out.println("5Gb Youtube");
+            case "M":
+                System.out.println("Whatsapp e Instragram grátis");
+            case "B":
+                System.out.println("100 minutos de ligação");
+        }
+    }
+}
